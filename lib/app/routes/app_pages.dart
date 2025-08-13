977aa3d911f5b030caac44c344773965af760273
lelahnaomie <124450076+lelahnaomie@users.noreply.github.com>
2025-08-13 19:43:21 +0100

    create files for auth

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,23 @@
 import 'package:get/get.dart';
 
+import '../modules/ForgotPassword/bindings/forgot_password_binding.dart';
+import '../modules/ForgotPassword/views/forgot_password_view.dart';
+import '../modules/Login/bindings/login_binding.dart';
+import '../modules/Login/views/login_view.dart';
+import '../modules/OTP/bindings/otp_binding.dart';
+import '../modules/OTP/views/otp_view.dart';
+import '../modules/Onboarding/bindings/onboarding_binding.dart';
+import '../modules/Onboarding/views/onboarding_view.dart';
+import '../modules/Register/bindings/register_binding.dart';
+import '../modules/Register/views/register_view.dart';
+import '../modules/ResetPassword/bindings/reset_password_binding.dart';
+import '../modules/ResetPassword/views/reset_password_view.dart';
+import '../modules/Splash/bindings/splash_binding.dart';
+import '../modules/Splash/views/splash_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/welcomer/bindings/welcomer_binding.dart';
+import '../modules/welcomer/views/welcomer_view.dart';
 
 part 'app_routes.dart';
 
@@ -15,6 +31,46 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.WELCOMER,
+      page: () => const WelcomerView(),
+      binding: WelcomerBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => const SplashView(),
+      binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.REGISTER,
+      page: () => const RegisterView(),
+      binding: RegisterBinding(),
+    ),
+    GetPage(
+      name: _Paths.FORGOT_PASSWORD,
+      page: () => const ForgotPasswordView(),
+      binding: ForgotPasswordBinding(),
+    ),
+    GetPage(
+      name: _Paths.OTP,
+      page: () => const OtpView(),
+      binding: OtpBinding(),
+    ),
+    GetPage(
+      name: _Paths.RESET_PASSWORD,
+      page: () => const ResetPasswordView(),
+      binding: ResetPasswordBinding(),
+    ),
+    GetPage(
+      name: _Paths.ONBOARDING,
+      page: () => const OnboardingView(),
+      binding: OnboardingBinding(),
     ),
   ];
 }
